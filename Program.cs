b187515527d9528b93e2b16d324cbfff804f35ef
Kamil <kamilkolodziejczak95@gmail.com>
2021-11-13 12:47:12 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MentalEffect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
